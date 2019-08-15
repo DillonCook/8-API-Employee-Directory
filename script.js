462437f8999756b29907926d48dedf21ef3dffc9
Dillon Cook <dillonlee.cook@gmail.com>
2019-08-15 12:44:46 -0400

    Initial commit

---
+++ script.js
@@ -0,0 +1,79 @@
+// pull and dynamically create info from API and add to grid layout
+
+fetch('https://randomuser.me/api/?results=12&nat=us,au')
+  .then(results => {
+    return results.json();
+  })
+  .then(results => {
+    // console.log(JSON.stringify(results));
+    results.results.forEach(person => {
+
+      let name = `
+      <h1 class="name">${person.name.first + " " + person.name.last}</h1>
+      `;
+      let pic = `
+        <img class="user-image" src="${person.picture.large}">
+      `;
+      let email = `
+        <p class="email">${person.email}</p>
+      `;
+      let state = `
+        <p class="state">${person.location.state}</p>
+      `;
+      let phone = `
+        <p class="phone">${person.cell}</p>
+      `;
+      let address = `
+        <p class="address">${person.location.street + ", " + person.location.city + ", " + person.location.state + " " + person.location.postcode}</p>
+      `;
+      let birthday = `
+        <p class="birthday">${"Birthday: " + person.dob.date.slice(0, 10)}</p>
+      `;
+
+
+      $("main").append(`
+      <div class="user">
+        ${pic}
+        <div class="user-info">${name + email + state + phone + address + birthday}</div>
+      </div>
+      `);
+
+
+      let cards = document.querySelectorAll('.user');
+
+      cards.forEach(card => {
+        card.addEventListener('click', (e) => {
+          let modal = document.createElement('div');
+          let close = document.createElement('button');
+          let main = document.querySelector('main');
+          modal.classList.add("modal");
+          close.classList.add('close');
+          close.innerHTML = "x";
+          modal.innerHTML = card.innerHTML;
+          modal.append(close);
+          main.append(modal);
+
+        });
+      });
+
+    });
+  })
+
+// Create modal on click
+
+document.addEventListener('click', (e) => {
+  let modal = document.querySelectorAll('.modal');
+  if(e.target.classList == 'close') {
+    modal.forEach(mod => {
+      mod.style.display = "none";
+    });
+    
+  }
+
+})
+  
+
+
+
+
+  
